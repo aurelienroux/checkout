05d9ce5258753defc048ebad7f2decc9647c95ae
Aurélien Roux <aureroux13@gmail.com>
2021-05-28 19:57:49 -0400

    functional attribute selector

--- src/components/AttributeSelector.vue
+++ src/components/AttributeSelector.vue
@@ -1,8 +1,16 @@
 <template>
   <div>
-    {{ variant.name }}
+    {{ attribute }}
+    {{ selected }}
 
-    <button v-for="(value, index) in variant.values" :key="index">{{ value }}</button>
+    <button
+      :class="{ selected: value === selected }"
+      v-for="(value, index) in data"
+      :key="index"
+      @click="updateData({ attribute, value })"
+    >
+      {{ value }}
+    </button>
   </div>
 </template>
 
@@ -11,6 +19,21 @@ import Vue from 'vue'
 
 export default Vue.extend({
   name: 'AttributeSelector',
-  props: ['variant']
+  props: ['attribute', 'data', 'selected'],
+  methods: {
+    updateData(data) {
+      this.$emit('updateData', data)
+    }
+  }
 })
 </script>
+
+<style lang="scss" scoped>
+div {
+  border: 1px solid;
+}
+
+.selected {
+  background: blueviolet;
+}
+</style>
--- src/components/ProductTile.vue
+++ src/components/ProductTile.vue
@@ -2,14 +2,31 @@
   <div>
     {{ productInfo.name }}
     <div>selectedVariantId: {{ selectedVariantId }}</div>
+    <!-- <div>currentSize {{ currentSize }}</div> -->
     <div>Price : ${{ currentPrice }}</div>
-    <!-- <div>laSoluce: {{ possibleVariants }}</div> -->
-    <AttributeSelector v-for="(variant, index) in possibleVariants" :key="index" :variant="variant" />
+    <div>selected size {{ selectedSize }}</div>
+    <div>selected fabric {{ selectedFabric }}</div>
+    <!-- <div>possible Sizes {{ possibleSizes }}</div> -->
+    <!-- <div>possible Fabrics {{ possibleFabrics }}</div> -->
+    <!-- <AttributeSelector v-for="(variant, index) in possibleVariants" :key="index" :variant="variant" /> -->
+    <AttributeSelector
+      attribute="Size"
+      :data="possibleSizes"
+      :selected="selectedSize"
+      @updateData="updateData"
+    />
+    <AttributeSelector
+      attribute="Fabric"
+      :data="possibleFabrics"
+      :selected="selectedFabric"
+      @updateData="updateData"
+    />
     <hr />
   </div>
 </template>
 
 <script lang="ts">
+import { variant } from '@/types'
 import Vue from 'vue'
 import AttributeSelector from './AttributeSelector'
 
@@ -19,38 +36,75 @@ export default Vue.extend({
   components: {
     AttributeSelector
   },
+  data() {
+    return {
+      selectedVariantId: this.productInfo.defaultVariantId as string,
+      selectedSize: 999,
+      selectedFabric: 666
+    }
+  },
   computed: {
-    selectedVariantId() {
-      return this.productInfo.defaultVariantId
-    },
     currentPrice() {
-      const selectedVariant = this.productInfo.variants.filter(variant => {
-        return variant.id === this.selectedVariantId
+      const selectedVariant = this.productInfo.variants.filter((variant: variant) => {
+        return variant.id === this.selectedVariantId.toString()
       })
       return selectedVariant[0].price
     },
-    possibleVariants() {
-      const result = []
-
-      this.productInfo.variants.forEach(({ attributes }) => {
-        attributes.forEach(attribute => {
-          const { name, value } = attribute
-          const index = result.findIndex(el => el['name'] === name)
-
-          if (index !== -1) {
-            if (result[index].values.includes(value)) return
-
-            result[index].values.push(value)
-          } else {
-            result.push({
-              name: name,
-              values: [value]
-            })
-          }
-        })
+    currentSize() {
+      const selectedVariant = this.productInfo.variants.filter((variant: variant) => {
+        return variant.id === this.selectedVariantId.toString()
       })
+      return selectedVariant[0].attributes[0].value
+    },
+    // possibleVariants() {
+    //   const result = []
 
+    //   this.productInfo.variants.forEach(({ attributes }) => {
+    //     attributes.forEach(attribute => {
+    //       const { name, value } = attribute
+    //       const index = result.findIndex(el => el['name'] === name)
+
+    //       if (index !== -1) {
+    //         if (result[index].values.includes(value)) return
+
+    //         result[index].values.push(value)
+    //       } else {
+    //         result.push({
+    //           name: name,
+    //           values: [value]
+    //         })
+    //       }
+    //     })
+    //   })
+
+    //   return result
+    // },
+    possibleSizes() {
+      const result: string[] = []
+
+      this.productInfo.variants.forEach((variant: variant) => {
+        if (result.includes(variant.attributes[0].value)) return
+
+        result.push(variant.attributes[0].value)
+      })
       return result
+    },
+    possibleFabrics() {
+      const result: string[] = []
+
+      this.productInfo.variants.forEach((variant: variant) => {
+        if (result.includes(variant.attributes[1].value)) return
+
+        result.push(variant.attributes[1].value)
+      })
+      return result
+    }
+  },
+  methods: {
+    updateData(data) {
+      console.log(data)
+
+      this[`selected${data.attribute}`] = data.value
     }
   }
 })
