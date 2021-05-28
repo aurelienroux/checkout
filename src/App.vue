b19ea4079640b51d4573f0e74078fc7ce6015201
Aurélien Roux <aureroux13@gmail.com>
2021-05-27 20:45:08 -0400

    app grid layout

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,40 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <div class="grid">
+      <div class="hero">hero</div>
+      <div class="hero-text">hero text</div>
+      <div class="cart">
+        cart content
+
+        <p>test</p>
+        <p>test</p>
+        <p>test</p>
+        <p>test</p>
+        <p>test</p>
+        <p>test</p>
+        <p>test</p>
+        <p>test</p>
+        <p>test</p>
+        <p>test</p>
+        <p>test</p>
+        <p>test</p>
+        <p>test</p>
+        <p>test</p>
+        <p>test</p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam, quia repellat. Voluptates, animi eum
+        explicabo dolores sequi, obcaecati temporibus quod est labore ratione cupiditate quas nulla,
+        aspernatur sint ut eaque!
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
 
 export default Vue.extend({
   name: 'App',
-  components: {
-    HelloWorld
-  },
+
   mounted() {
     this.$store.dispatch('getProducts')
   }
@@ -21,12 +42,45 @@ export default Vue.extend({
 </script>
 
 <style lang="scss">
+html {
+  font-size: 10px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  background: rebeccapurple;
+}
+
+.grid {
+  display: grid;
+  grid-template-rows: 33rem 11rem auto;
+  grid-template-columns: 1fr 100rem 1fr;
+}
+
+.hero {
+  background: green;
+  grid-column: 1 / 4;
+  grid-row: 1 / 3;
+}
+
+.hero-text {
+  background: salmon;
+  grid-column: 2 / 3;
+  grid-row: 1 / 2;
+}
+
+.cart {
+  background: rgb(3, 133, 143);
+  grid-column: 2 / 3;
+  grid-row: 2 / 4;
+  padding-top: 1.5rem;
+}
+
+div {
+  border: 1px solid blue;
 }
 </style>
